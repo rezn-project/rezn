1f93390506501fee0e19f01bdd6e1832a7740616
Andrea Mancuso <andreamancuso@users.noreply.github.com>
2025-07-02 23:24:20 +0200

    Moved route builder function to separate file (#17)

--- rezn-runtime/src/main.rs
+++ rezn-runtime/src/main.rs
@@ -1,24 +1,14 @@
 mod orqos_client;
 mod reconcile;
 
+mod router;
 mod routes;
 
 use std::env;
 use std::sync::Arc;
 
-use crate::{
-    reconcile::reconcile,
-    routes::{
-        apply::apply_handler,
-        state::{get_state_handler, get_state_raw_handler},
-    },
-};
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use crate::{reconcile::reconcile, router::build_router};
 use sled::Db;
-use utoipa::OpenApi;
 
 use std::sync::atomic::{AtomicBool, Ordering};
 use tokio::{net::TcpListener, sync::mpsc};
@@ -27,29 +17,6 @@ use tokio::{net::TcpListener, sync::mpsc};
 struct AppState {
     db: Arc<Db>,
     orqos: Arc<orqos_client::OrqosClient>,
-}
-
-#[derive(OpenApi)]
-#[openapi(
-    info(description = "Rezn Api"),
-    paths(
-        crate::routes::apply::apply_handler,
-        crate::routes::state::get_state_handler,
-        crate::routes::state::get_state_raw_handler,
-    )
-)]
-struct ApiDoc;
-
-pub(crate) fn build_router(app: Arc<AppState>) -> Router {
-    Router::new()
-        .route("/apply", post(apply_handler))
-        .route("/state", get(get_state_handler))
-        .route("/state/raw", get(get_state_raw_handler))
-        .with_state(app)
-        .merge(
-            utoipa_swagger_ui::SwaggerUi::new("/swagger")
-                .url("/api/openapi.json", ApiDoc::openapi()),
-        )
 }
 
 #[tokio::main(flavor = "multi_thread")]
---
+++ rezn-runtime/src/router.rs
@@ -0,0 +1,38 @@
+use std::sync::Arc;
+
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use utoipa::OpenApi;
+
+use crate::{
+    routes::{
+        apply::apply_handler,
+        state::{get_state_handler, get_state_raw_handler},
+    },
+    AppState,
+};
+
+#[derive(OpenApi)]
+#[openapi(
+    info(description = "Rezn Api"),
+    paths(
+        crate::routes::apply::apply_handler,
+        crate::routes::state::get_state_handler,
+        crate::routes::state::get_state_raw_handler,
+    )
+)]
+struct ApiDoc;
+
+pub(crate) fn build_router(app: Arc<AppState>) -> Router {
+    Router::new()
+        .route("/apply", post(apply_handler))
+        .route("/state", get(get_state_handler))
+        .route("/state/raw", get(get_state_raw_handler))
+        .with_state(app)
+        .merge(
+            utoipa_swagger_ui::SwaggerUi::new("/swagger")
+                .url("/api/openapi.json", ApiDoc::openapi()),
+        )
+}
